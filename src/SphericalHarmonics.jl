d8838320bd87a2f41cb7a4f235ae407d7c8d6808
Sheehan Olver <solver@mac.com>
2020-03-16 20:14:10 +0000

    Swap φ and θ

--- src/SphericalHarmonics.jl
+++ src/SphericalHarmonics.jl
@@ -23,43 +23,43 @@ abstract type AbstractSphericalCoordinate{T} <: StaticVector{3,T} end
 norm(::AbstractSphericalCoordinate{T}) where T = real(one(T))
 
 struct SphericalCoordinate{T} <: StaticVector{3,T}
-    θ::T
     φ::T
+    θ::T
 end
 
 struct ZSphericalCoordinate{T} <: StaticVector{3,T}
-    θ::T
+    φ::T
     z::T
-    function ZSphericalCoordinate{T}(θ::T, z::T) where T 
+    function ZSphericalCoordinate{T}(φ::T, z::T) where T 
         -1 ≤ z ≤ 1 || throw(ArgumentError("z must be between -1 and 1"))
-        new{T}(θ, z)
+        new{T}(φ, z)
     end
 end
-ZSphericalCoordinate(θ::T, z::V) where {T,V} = ZSphericalCoordinate{promote_type(T,V)}(θ,z)
-ZSphericalCoordinate(S::SphericalCoordinate) = ZSphericalCoordinate(S.θ, cos(S.φ))
-ZSphericalCoordinate{T}(S::SphericalCoordinate) where T = ZSphericalCoordinate{T}(S.θ, cos(S.φ))
+ZSphericalCoordinate(φ::T, z::V) where {T,V} = ZSphericalCoordinate{promote_type(T,V)}(φ,z)
+ZSphericalCoordinate(S::SphericalCoordinate) = ZSphericalCoordinate(S.φ, cos(S.θ))
+ZSphericalCoordinate{T}(S::SphericalCoordinate) where T = ZSphericalCoordinate{T}(S.φ, cos(S.θ))
 
-SphericalCoordinate(S::ZSphericalCoordinate) = SphericalCoordinate(S.θ, acos(S.z))
-SphericalCoordinate{T}(S::ZSphericalCoordinate) where T = SphericalCoordinate{T}(S.θ, acos(S.z))
+SphericalCoordinate(S::ZSphericalCoordinate) = SphericalCoordinate(S.φ, acos(S.z))
+SphericalCoordinate{T}(S::ZSphericalCoordinate) where T = SphericalCoordinate{T}(S.φ, acos(S.z))
 
 
 function getindex(S::SphericalCoordinate, k::Int)
-    k == 1 && return sin(S.φ) * cos(S.θ)
-    k == 2 && return sin(S.φ) * sin(S.θ)
-    k == 3 && return cos(S.φ)
+    k == 1 && return sin(S.θ) * cos(S.φ)
+    k == 2 && return sin(S.θ) * sin(S.φ)
+    k == 3 && return cos(S.θ)
     throw(BoundsError(S, k))
 end
 function getindex(S::ZSphericalCoordinate, k::Int) 
-    k == 1 && return sqrt(1-S.z^2) * cos(S.θ)
-    k == 2 && return sqrt(1-S.z^2) * sin(S.θ)
+    k == 1 && return sqrt(1-S.z^2) * cos(S.φ)
+    k == 2 && return sqrt(1-S.z^2) * sin(S.φ)
     k == 3 && return S.z
     throw(BoundsError(S, k))
 end
 
-convert(::Type{SVector{3,T}}, S::SphericalCoordinate) where T = SVector{3,T}(sin(S.φ)*cos(S.θ), sin(S.φ)*sin(S.θ), cos(S.φ))
-convert(::Type{SVector{3,T}}, S::ZSphericalCoordinate) where T = SVector{3,T}(sqrt(1-S.z^2)*cos(S.θ), sqrt(1-S.z^2)*sin(S.θ), S.z)
-convert(::Type{SVector{3}}, S::SphericalCoordinate) = SVector(sin(S.φ)*cos(S.θ), sin(S.φ)*sin(S.θ), cos(S.φ))
-convert(::Type{SVector{3}}, S::ZSphericalCoordinate) = SVector(sqrt(1-S.z^2)*cos(S.θ), sqrt(1-S.z^2)*sin(S.θ), S.z)
+convert(::Type{SVector{3,T}}, S::SphericalCoordinate) where T = SVector{3,T}(sin(S.θ)*cos(S.φ), sin(S.θ)*sin(S.φ), cos(S.θ))
+convert(::Type{SVector{3,T}}, S::ZSphericalCoordinate) where T = SVector{3,T}(sqrt(1-S.z^2)*cos(S.φ), sqrt(1-S.z^2)*sin(S.φ), S.z)
+convert(::Type{SVector{3}}, S::SphericalCoordinate) = SVector(sin(S.θ)*cos(S.φ), sin(S.θ)*sin(S.φ), cos(S.θ))
+convert(::Type{SVector{3}}, S::ZSphericalCoordinate) = SVector(sqrt(1-S.z^2)*cos(S.φ), sqrt(1-S.z^2)*sin(S.φ), S.z)
 
 convert(::Type{SphericalCoordinate}, S::ZSphericalCoordinate) = SphericalCoordinate(S)
 convert(::Type{SphericalCoordinate{T}}, S::ZSphericalCoordinate) where T = SphericalCoordinate{T}(S)
@@ -76,7 +76,7 @@ function getindex(S::SphericalHarmonic, x::ZSphericalCoordinate, K::BlockIndex{1
     ℓ = Int(block(K))
     k = blockindex(K)
     m = k-ℓ
-    exp(im*m*x.θ) * associatedlegendre(abs(m))[x.z,ℓ]
+    exp(im*m*x.φ) * associatedlegendre(abs(m))[x.z,ℓ]
 end
 
 getindex(S::SphericalHarmonic, x::StaticVector{3}, K::BlockIndex{1}) = 
