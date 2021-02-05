e65d3f3c62c0321ccdfffdba6ed0c0882c3d2e27
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-02-05 22:29:11 +0000

    unify Laplacian implementation
    unifies both laplacian^k and real and complex version into the same function

--- src/laplace.jl
+++ src/laplace.jl
@@ -9,20 +9,9 @@ Laplacian(axis) = Laplacian{eltype(axis)}(axis)
 axes(D::Laplacian) = (D.axis, D.axis)
 ==(a::Laplacian, b::Laplacian) = a.axis == b.axis
 
-^(D::Laplacian, k::Integer) = ApplyQuasiArray(^, D, k)
+^(D::Laplacian, k::Integer) = ApplyQuasiMatrix(^, D, k)
 
-@simplify function *(Δ::Laplacian, P::SphericalHarmonic)
+@simplify function *(Δ::Laplacian, P::AbstractSphericalHarmonic)
      # Spherical harmonics are the eigenfunctions of the Laplace operator on the unit sphere
      P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
-end
-@simplify function *(Δ::Laplacian, P::RealSphericalHarmonic)
-    P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
-end
-
-# this does Δ^k and should probably work automatically but currently doesn't.
-function *(D::ApplyQuasiArray{SphericalCoordinate{T},2,typeof(^),Tuple{Laplacian{SphericalCoordinate{T},DomainSets.FixedUnitSphere{StaticArrays.SArray{Tuple{3},T,1,3}}},A}}, P::SphericalHarmonic) where {T,A<:Integer}
-    return P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2).^D.args[2], 1:2:∞)))
-end
-function *(D::ApplyQuasiArray{SphericalCoordinate{T},2,typeof(^),Tuple{Laplacian{SphericalCoordinate{T},DomainSets.FixedUnitSphere{StaticArrays.SArray{Tuple{3},T,1,3}}},A}}, P::RealSphericalHarmonic) where {T,A<:Integer}
-    return P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2).^D.args[2], 1:2:∞)))
 end
