11dcb07ee14932e21881408d6bacd0e55e47647f
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-02-05 01:19:39 +0000

    laplace operator

---
+++ src/laplace.jl
@@ -0,0 +1,22 @@
+struct Laplacian{T,D} <: LazyQuasiMatrix{T}
+    axis::Inclusion{T,D}
+end
+
+Laplacian{T}(axis::Inclusion{<:Any,D}) where {T,D} = Laplacian{T,D}(axis)
+Laplacian{T}(domain) where T = Laplacian{T}(Inclusion(domain))
+Laplacian(axis) = Laplacian{eltype(axis)}(axis)
+
+axes(D::Laplacian) = (D.axis, D.axis)
+==(a::Laplacian, b::Laplacian) = a.axis == b.axis
+copy(D::Laplacian) = Laplacian(copy(D.axis), D.k)
+
+^(D::Laplacian, k::Integer) = ApplyQuasiArray(^, D, k)
+
+@simplify function *(Δ::Laplacian, P::SphericalHarmonic)
+     # Spherical harmonics are the eigenfunctions of the Laplace operator on the unit sphere
+     P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
+end
+
+@simplify function *(Δ::Laplacian, P::RealSphericalHarmonic)
+    P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
+end
