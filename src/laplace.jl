94e764bb37d9363f7b93e0353de8f53c2eb453b5
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-02-06 03:53:06 +0000

    remove redundant power definition
    raising to integer power can be dealt with in QuasiArrays.jl

--- src/laplace.jl
+++ src/laplace.jl
@@ -10,8 +10,6 @@ axes(D::Laplacian) = (D.axis, D.axis)
 ==(a::Laplacian, b::Laplacian) = a.axis == b.axis
 copy(D::Laplacian) = Laplacian(copy(D.axis))
 
-^(D::Laplacian, k::Integer) = ApplyQuasiMatrix(^, D, k)
-
 @simplify function *(Δ::Laplacian, P::AbstractSphericalHarmonic)
      # Spherical harmonics are the eigenfunctions of the Laplace operator on the unit sphere
      P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
