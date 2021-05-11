2fa1b74b4117fa460cc60c1d50ddb0b0a02f7361
Timon Salar Gutleb <timon.gutleb@gmail.com>
2021-05-11 16:43:03 +0100

    Powers of negative Laplacians (#32)
    * import export
    
    * abs power laplacian struct
    
    * tests
    
    * v0.1

--- src/laplace.jl
+++ src/laplace.jl
@@ -1,3 +1,5 @@
+# Laplacian
+
 struct Laplacian{T,D} <: LazyQuasiMatrix{T}
     axis::Inclusion{T,D}
 end
@@ -14,3 +16,21 @@ copy(D::Laplacian) = Laplacian(copy(D.axis))
      # Spherical harmonics are the eigenfunctions of the Laplace operator on the unit sphere
      P * Diagonal(mortar(Fill.((-(0:∞)-(0:∞).^2), 1:2:∞)))
 end
+
+# Negative fractional Laplacian (-Δ)^α or equiv. abs(Δ)^α
+
+struct AbsLaplacianPower{T,D,A} <: LazyQuasiMatrix{T}
+    axis::Inclusion{T,D}
+    α::A
+end
+
+AbsLaplacianPower{T}(axis::Inclusion{<:Any,D},α) where {T,D} = AbsLaplacianPower{T,D,typeof(α)}(axis,α)
+
+axes(D:: AbsLaplacianPower) = (D.axis, D.axis)
+==(a:: AbsLaplacianPower, b:: AbsLaplacianPower) = a.axis == b.axis && a.α == b.α
+copy(D:: AbsLaplacianPower) = AbsLaplacianPower(copy(D.axis), D.α)
+
+abs(Δ::Laplacian) = AbsLaplacianPower(axes(Δ,1),1)
+-(Δ::Laplacian) = abs(Δ)
+
+^(D::AbsLaplacianPower, k) = AbsLaplacianPower(D.axis, D.α*k)
