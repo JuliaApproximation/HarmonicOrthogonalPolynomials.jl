f5ef2dcbd236ba92ed6934f188e610da4b309376
Sheehan Olver <solver@mac.com>
2026-06-17 22:25:14 +0100

    pointchoice -> choice (#104)
    * pointchoice -> choice
    
    * Update HarmonicOrthogonalPolynomials.jl

--- src/HarmonicOrthogonalPolynomials.jl
+++ src/HarmonicOrthogonalPolynomials.jl
@@ -3,9 +3,9 @@ using FastTransforms, LinearAlgebra, ClassicalOrthogonalPolynomials, ContinuumAr
         BlockArrays, BlockBandedMatrices, InfiniteArrays, StaticArrays, QuasiArrays, Base, SpecialFunctions, FillArrays
 import Base: OneTo, oneto, axes, getindex, convert, to_indices, tail, eltype, *, ==, ^, copy, -, abs, resize!, zero
 import BlockArrays: block, blockindex, unblock, BlockSlice
-import DomainSets: indomain, Sphere
+import DomainSets: indomain, Sphere, choice
 import LinearAlgebra: norm, factorize
-import QuasiArrays: to_quasi_index, SubQuasiArray, *, AbstractQuasiVecOrMat, pointchoice, Inclusion
+import QuasiArrays: to_quasi_index, SubQuasiArray, *, AbstractQuasiVecOrMat, Inclusion
 import ContinuumArrays: TransformFactorization, @simplify, ProjectionFactorization, plan_grid_transform, plan_transform, grid, grid_layout, plotgrid_layout,
                         AbstractBasisLayout, MemoryLayout, abslaplacian, laplacian, AbstractDifferentialQuasiMatrix, operatorcall, similaroperator, SubBasisLayout,
                         ApplyLayout, arguments, ExpansionLayout, basis_axes, grammatrix
@@ -26,9 +26,8 @@ include("coordinates.jl")
 # roughly try to double the computational time each iteration
 increasingtruncations(::BlockedOneTo{Int,<:RangeCumsum{Int,<:AbstractRange}}) = broadcast(n -> Block.(oneto((2^(n ÷ 2)) ÷ 2)), 4:2:∞)
 
-
 checkpoints(S::UnitSphere) = [SphericalCoordinate{eltype(eltype(S))}(0.1,0.2), SphericalCoordinate{eltype(eltype(S))}(0.3,0.4)]
-pointchoice(S::UnitSphere) = SphericalCoordinate{eltype(eltype(S))}(0,0)
+choice(S::Inclusion{<:Any,<:UnitSphere}) = SphericalCoordinate{eltype(eltype(S))}(0,0)
 
 include("sphericalharmonics.jl")
 include("laplace.jl")
